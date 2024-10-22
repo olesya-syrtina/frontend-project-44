42dea943fab8c2b937fed5e64559d4110db1e9d9
Олеся Сыртина <olesademcenko87091@gmail.com>
2024-10-22 15:47:26 +0300

    Добавление игры "Простое ли число?"

---
+++ bin/brain-prime.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+/* eslint-disable indent */
+
+import startPrime from '../src/games/brain-prime.js';
+
+startPrime();
---
+++ src/games/brain-prime.js
@@ -0,0 +1,22 @@
+import mainEngine from '../index.js';
+
+const description = 'Find theAnswer "yes" if given number is prime. Otherwise answer "no".';
+
+const getData = () => {
+  const randomNum = Math.floor(Math.random() * 20) + 1;
+  const question = randomNum;
+  let rightAnswer = 'yes';
+
+  for (let i = 2; i < randomNum / 2; i += 1) {
+    if (randomNum % i === 0) {
+      rightAnswer = 'no';
+      break;
+    }
+  }
+
+  return [question, rightAnswer];
+};
+
+const startPrime = () => mainEngine(getData, description);
+
+export default startPrime;
